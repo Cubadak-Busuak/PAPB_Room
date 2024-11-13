768411a108ebfcaf87d9efa5d6edd839064000d0
Javed <aqiljaved110903@gmail.com>
2024-11-13 15:34:41 +0700

    Progress

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,4 +86,9 @@ dependencies {
     implementation ("androidx.compose.material3:material3:1.0.0")
     implementation ("androidx.compose.ui:ui:1.4.0")
     implementation ("androidx.activity:activity-compose:1.6.0")
+
+    //Room
+    implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
+    ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
+    implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
 }
